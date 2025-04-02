61a54d04ba3c6902e9d58a7acdfd28c4c6bae986
Rohit Bernard <rohit@ezdubs.ai>
2025-04-02 16:17:53 -0700

    hopeful android fix

--- flutter/flutter/android/src/main/java/com/arthenica/ffmpegkit/flutter/FFmpegKitFlutterPlugin.java
+++ flutter/flutter/android/src/main/java/com/arthenica/ffmpegkit/flutter/FFmpegKitFlutterPlugin.java
@@ -144,6 +144,22 @@ public class FFmpegKitFlutterPlugin implements FlutterPlugin, ActivityAware, Met
     private EventChannel.EventSink eventSink;
     private final FFmpegKitFlutterMethodResultHandler resultHandler;
 
+    static {
+        try {
+            System.loadLibrary("avcodec");
+            System.loadLibrary("avfilter");
+            System.loadLibrary("avformat");
+            System.loadLibrary("avutil");
+            System.loadLibrary("swresample");
+            System.loadLibrary("swscale");
+            System.loadLibrary("ffmpegkit");
+            // ...any others if you renamed them or use more
+        } catch (UnsatisfiedLinkError e) {
+            Log.e("FFmpegKitPlugin", "Native library load failed", e);
+        }
+    }
+
+
     public FFmpegKitFlutterPlugin() {
         this.logsEnabled = new AtomicBoolean(false);
         this.statisticsEnabled = new AtomicBoolean(false);
